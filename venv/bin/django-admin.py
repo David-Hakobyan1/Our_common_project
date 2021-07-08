e24d374aade12c5d50fbf0238a792b4ffb446df5
Mamikon Papikyan <mamikon_papikyan@instigatedesign.com>
2021-07-08 21:17:21 +0400

    first commit

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,5 +1,21 @@
-#!/home/David/python/papkaDjango/venv/bin/python3
+#!/home/mamikon/Desktop/vahan/OurProject/venv/bin/python
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
 from django.core import management
 
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
 if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
     management.execute_from_command_line()
