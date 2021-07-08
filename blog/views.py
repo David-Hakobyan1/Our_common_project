ee1441cbc85345a9bece725341a839bc5577adc2
Arsen Gevorgyan <arsen.gevorgyan.95@bk.ru>
2021-07-08 23:17:36 +0400

    new style

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'blog/home.html'
 
 # Create your views here.
